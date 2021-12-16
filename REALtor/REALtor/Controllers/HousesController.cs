65143ba9de2e7a46aa74e5fb55aad454cb1ffafd
Yuliya Zhuravleva <zhuravleva_02@mail.ru>
2021-12-16 15:20:54 +0400

    Reorganised vs project, add House Controller

---
+++ REALtor/REALtor/Controllers/HousesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using REALtor.Data.Interfaces;
+
+namespace REALtor.Controllers
+{
+    public class HousesController: Controller
+    {
+        private readonly IAllHouses _allHouses;
+        public HousesController(IAllHouses iAllHouses)
+        {
+            _allHouses = iAllHouses;
+        }
+        public ViewResult ListAllHouses()
+        {
+            ViewBag.Title = "Страница с недвижимостью";
+            return View();
+        }
+    }
+}
